0d51c5268a74bf01e974f8184c4fd0d807db3abb
Sergio Aravena <sergt26@hotmail.com>
2017-06-14 15:25:11 -0400

    updated the eventbrite api

--- app/controllers/concerns/event_brite_api.rb
+++ app/controllers/concerns/event_brite_api.rb
@@ -16,25 +16,4 @@ class EventBriteApi
       }
     )
   end
-
 end
-# auth = {username: 'user', password: 'pass'}
-#
-# result = HTTParty.get(
-#   "https://unicom24.ru/api/partners/requests/v1/locality/",
-#   basic_auth: auth
-# )
-#
-#
-#
-#
-# "https://www.eventbriteapi.com/v3/users/me/owned_events/",
-#    headers = {
-#        "Authorization": "Bearer SESXYS4X3FJ5LHZRWGKQ",
-#    },
-
-# HTTParty.post(
-#   "https://www.acb.com/api/v2/market/LTC_BTC/",
-#   :query => query,
-#   :headers => headers
-# )
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,8 @@
 class EventsController < ApplicationController
 
   def index
-    @event_brite_events = EventBriteApi.new("https://www.eventbriteapi.com/v3/events/search/", {q: 'Miami'}).event_getter
+    @event_brite_events = EventBriteApi.new("https://www.eventbriteapi.com/v3/events/search/",
+    {q: 'Miami'}).event_getter
   end
-
 
 end
