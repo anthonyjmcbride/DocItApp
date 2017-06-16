e2fef4af5dbf734960dcfa10fd845c63d3781100
Sergio Aravena <sergt26@hotmail.com>
2017-06-15 20:46:59 -0400

    mergin branches

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,16 +7,15 @@ class EventsController < ApplicationController
     end
 
     def search
-    @event_brite_events = Event.get_info(params[:q])
+     @event_brite_events = Event.get_info(params[:q])
       respond_to do |format|
         format.json { render partial: 'list' }
-      end
-
-    else
-      query = "Miami"
     end
-    @event_brite_events = EventBriteApi.new("https://www.eventbriteapi.com/v3/events/search/",{q: query }).event_getter
-    respond_to do |format|
-      format.json { render 'list' }
+    # else
+    #   query = "Miami"
+    # end
+    # @event_brite_events = EventBriteApi.new("https://www.eventbriteapi.com/v3/events/search/",{q: query }).event_getter
+    # respond_to do |format|
+    #   format.json { render 'list' }
     end
   end
