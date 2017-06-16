45444826743af9989bd9095da35f114bb9d9bfaf
Raphael Machin <rmachin75@gmail.com>
2017-06-15 20:02:32 -0400

    working version heroku and api evenbrite

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,13 +4,6 @@ class EventsController < ApplicationController
 
       @event_brite_events = Event.get_info(params[:q])
 
-      # if params[:q].present?
-      #   query = params[:q]
-      # else
-      #   query = "Miami"
-      # end
-      # @event_brite_events = EventBriteApi.new("https://www.eventbriteapi.com/v3/events/search/",
-      # {q: query }).event_getter
     end
 
     def search
