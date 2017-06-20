cfbac9cbff0707bce6cc480385b7427dc9f0d17a
“Anthony <anthonyjmcbride@icloud.com>
2017-06-20 12:10:17 -0400

    resolving merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170617213515) do
-=======
-ActiveRecord::Schema.define(version: 20170617233105) do
->>>>>>> dfc2eac867d7be703f16c18d955701245c404f6c
-=======
+
 ActiveRecord::Schema.define(version: 20170619233205) do
 >>>>>>> 97242b0c7aedd67e50a8c88f30fb1a6e8ab7f892
 
