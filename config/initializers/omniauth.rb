f180a1c647a3606a6a93533d76c1bde56afdedc7
“Anthony <anthonyjmcbride@icloud.com>
2017-06-20 12:12:58 -0400

    resolving merge conflicts

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET']
 end
-<<<<<<< HEAD
-=======
+
 
 # :google_oauth2, ENV.fetch('GOOGLE_ID'), ENV.fetch('GOOGLE_SECRET')
->>>>>>> dfc2eac867d7be703f16c18d955701245c404f6c
