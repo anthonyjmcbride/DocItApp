835ad40c3a5b5af72f2f2b1a88ef884f3b6a2dd2
Raphael Machin <rmachin75@gmail.com>
2017-06-22 22:46:43 -0400

    versionnot working try to merge eventbrite and ticketmaster

--- app/controllers/concerns/ticket_master_api.rb
+++ app/controllers/concerns/ticket_master_api.rb
@@ -4,10 +4,11 @@ require 'json'
 class TicketMasterApi
 
 
-  def ticketm_getter(query)
+  def ticketm_getter(data)
     @url = "https://app.ticketmaster.com/discovery/v2/events.curl?apikey="
-    response = HTTParty.get(@url + "#{ENV['TICKET_API_KEY']}",city: "Miami")
+    response = HTTParty.get(@url + "#{ENV['TICKET_API_KEY']}",keyword:data)
     JSON.parse(response.parsed_response)
   end
 end
 # https://api.meetup.com/find/events/?key=4e66244a1965777745f47742e3d5240
+# https://app.ticketmaster.com/discovery/v2/events.json?apikey=MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :user
 
+  @current_user = User.find(1)
+  @location = @current_user.zipcode
 
   def self.get_info(params)
 
-    @current_user = User.find(1)
-    @location = @current_user.zipcode
       if params[:q].blank?
         query = @location
       else
@@ -14,46 +14,41 @@ class Event < ApplicationRecord
 
       res= EventBriteApi.new("https://www.eventbriteapi.com/v3/events/search/", { q:query }).event_getter
     end
-    # def self.get_ticket_info(params)
-    #   @current_user = User.find(1)
-    #   @location = @current_user.zipcode
-    #       params[:text] = params[:q]
-    #       # query = Hash.new
-    #       # query['text'] = params[:text] if params[:text].present?
-    #       # first load
-    #     if params[:text].blank?
-    #       query = @location
-    #     else
-    #       query = params[:text] +"%"+ params[:city]
-    #     end
-    #
-    #     res= TicketMasterApi.new("https://api.meetup.com/find/events/?key=").ticketm_getter(query)
-    #   end
+
 
       def self.get_ticket_info(params)
-        @current_user = User.find(1)
-        @location = @current_user.zipcode
-            params[:city] = params[:q]
-            # query = Hash.new
-            # query['city'] = params[:city] if params[:city].present?
-            # first load
-          if params[:city].blank?
-            query = "Miami"
+
+            @parameter = params[:q]
+          if params[:keyword].blank?
+            data = @location
           else
-            query = params[:city] +"%"+ params[:city]
+            data = @parameter +"%"+ params[:city]
           end
 
-    res= TicketMasterApi.new.ticketm_getter(query)
+    res= TicketMasterApi.new.ticketm_getter(data)
+
         end
 end
-# MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4&city=miami
-# https://app.ticketmaster.com/discovery/v2/
+
 
 # Examples
 # Get a list of all events in the United States https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4
 # Search for events sourced by Universe in the United States with keyword “devjam” https://app.ticketmaster.com/discovery/v2/events.json?keyword=devjam&source=universe&countryCode=US&apikey=MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4
 # Search for music events in the Los Angeles area https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4
 # Get a list of all events for Adele in Canada https://app.ticketmaster.com/discovery/v2/events.json?attractionId=K8vZ917Gku7&countryCode=CA&apikey=MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4
+
+# def self.get_ticket_info(params)
+#   @current_user = User.find(1)
+#   @location = @current_user.zipcode
+#       params[:text] = params[:q]
+#       # data = Hash.new
+#       # data['sport'] = params[:keyword] if params[:keyword].present?
+#       # first load
+#     if params[:text].blank?
+#       query = @location
+#     else
+#       query = params[:text] +"%"+ params[:city]
+#     end
 #
-# curl \
-# --include 'https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=MAPGpwLZjmR7v1hhu4cqAdlLAUPGdab4'
+#     res= TicketMasterApi.new("https://api.meetup.com/find/events/?key=").ticketm_getter(query)
+#   end
